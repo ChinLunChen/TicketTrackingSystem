703b252210c249b6b50c539db35a98f4796d83a7
chinlunchen <chinlunchen@gmail.com>
2022-06-14 16:16:29 +0800

    QA新增編輯刪除，RD解決，PM新增FeatureRequest。QA新增TestCase

--- TicketTrackingSystem/Controllers/HomeController.cs
+++ TicketTrackingSystem/Controllers/HomeController.cs
@@ -17,12 +17,6 @@ namespace TicketTrackingSystem.Controllers
     {
 
         NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=postgres;UserId=postgres;Password=729650");
-        //private readonly ILogger<HomeController> _logger;
-
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
 
         private readonly IMemoryCache memoryCache;
 
@@ -63,37 +57,12 @@ namespace TicketTrackingSystem.Controllers
             .SetSize(1024);
 
                 memoryCache.Set(MemoryCacheKey.sUserPermission, Model, cacheEntryOptions);
-                memoryCache.Set(MemoryCacheKey.sTest, sUser, cacheEntryOptions);
-                //var obj = memoryCache.Get(MemoryCacheKey.sTest);
-                ViewData["User"] = memoryCache.Get(MemoryCacheKey.sTest);
+                memoryCache.Set(MemoryCacheKey.sUser, sUser, cacheEntryOptions);
+                ViewData["User"] = memoryCache.Get(MemoryCacheKey.sUser);
+
                 conn.Open();
-
-                NpgsqlCommand cmd =
-                    new NpgsqlCommand("select * from BugList", conn);
-                //NpgsqlDataReader reader = command.ExecuteReader();
-                NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
-                DataSet _ds = new DataSet();
-                DataTable _dt = new DataTable();
-
-                da.Fill(_ds);
-                _dt = _ds.Tables[0];
-
-                for (int i = 0; i < _dt.Rows.Count; i++)
-                {
-                    BugListViewModel BugListModel = new BugListViewModel();
-                    BugListModel.ID = _dt.Rows[i]["ID"].ToString();
-                    BugListModel.Summary = _dt.Rows[i]["Summary"].ToString();
-                    BugListModel.Description = _dt.Rows[i]["Description"].ToString();
-                    BugListModel.Type = _dt.Rows[i]["Type"].ToString();
-                    BugListModel.Priority = _dt.Rows[i]["Priority"].ToString();
-                    BugListModel.Status = _dt.Rows[i]["Status"].ToString();
-                    BugListModel.CreateUser = _dt.Rows[i]["CreateUser"].ToString();
-                    BugListModel.CreateDate = _dt.Rows[i]["CreateDate"].ToString();
-                    BugListModel.EndUser = _dt.Rows[i]["EndUser"].ToString();
-                    BugListModel.EndDate = _dt.Rows[i]["EndDate"].ToString();
-                    BugModelList.Add(BugListModel);
-                }
-
+                BugModelList = GetBugLists();
+                conn.Close();
             }
             catch (Exception ex)
             {
@@ -103,7 +72,6 @@ namespace TicketTrackingSystem.Controllers
 
         public IActionResult Privacy()
         {
-
             return View();
         }
 
@@ -114,13 +82,168 @@ namespace TicketTrackingSystem.Controllers
 
         public IActionResult Edit()
         {
-            return View();
+            BugListViewModel Model = new BugListViewModel();
+            try
+            {
+                ViewData["bugID"] = Request.Path.Value.Split('/')[3];
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromSeconds(3600))
+            .SetSize(1024);
+                memoryCache.Set(MemoryCacheKey.sBugID, ViewData["bugID"].ToString(), cacheEntryOptions);
+
+                
+                NpgsqlCommand cmd = new NpgsqlCommand($"select * from BugList where id='{Request.Path.Value.Split('/')[3]}'", conn);
+
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                DataSet _ds = new DataSet();
+                DataTable _dt = new DataTable();
+
+                da.Fill(_ds);
+                _dt = _ds.Tables[0];
+
+                
+                Model.Summary = _dt.Rows[0]["summary"].ToString();
+                Model.Description = _dt.Rows[0]["description"].ToString();
+               
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View(Model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string Summary, string Description)
+        {
+            List<BugListViewModel> BugModelList = new List<BugListViewModel>();
+            try
+            {
+                ViewData["User"] = memoryCache.Get(MemoryCacheKey.sUser);
+
+
+                conn.Open();
+                string sBugID = memoryCache.Get(MemoryCacheKey.sBugID).ToString();
+                string sSQL = $"Update buglist set summary='{Summary}',description='{Description}' where id='{sBugID}'";
+                NpgsqlCommand cmd = new NpgsqlCommand(sSQL, conn);
+                cmd.ExecuteNonQuery();
+
+                BugModelList = GetBugLists();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+            return View("Index", BugModelList);
+        }
+
+
+        public IActionResult Resolve()
+        {
+            List<BugListViewModel> BugModelList = new List<BugListViewModel>();
+            try
+            {
+                ViewData["User"] = memoryCache.Get(MemoryCacheKey.sUser);
+                string sBugID = Request.Path.Value.Split('/')[3];
+
+                conn.Open();
+                string sSQL = $"Update buglist set status='Resolved' where id='{sBugID}'";
+                NpgsqlCommand cmd = new NpgsqlCommand(sSQL, conn);
+                cmd.ExecuteNonQuery();
+
+                BugModelList = GetBugLists();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View("Index", BugModelList);
         }
 
         public IActionResult Delete()
         {
+            List<BugListViewModel> BugModelList = new List<BugListViewModel>();
+            try
+            {
+                ViewData["User"] = memoryCache.Get(MemoryCacheKey.sUser);
+                string sBugID = Request.Path.Value.Split('/')[3];
+
+                conn.Open();
+                string sSQL = $"delete from buglist where id='{sBugID}'";
+                NpgsqlCommand cmd = new NpgsqlCommand(sSQL, conn);
+                cmd.ExecuteNonQuery();
+
+                BugModelList = GetBugLists();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View("Index", BugModelList);
+        }
+
+        public IActionResult Create_Bug()
+        {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create_Bug(string Summary, string Description, string Type, string Priority, string Status)
+        {
+            List<BugListViewModel> BugModelList = new List<BugListViewModel>();
+            try
+            {
+                ViewData["User"] = memoryCache.Get(MemoryCacheKey.sUser);
+                string sUser = memoryCache.Get(MemoryCacheKey.sUser).ToString();
+
+                conn.Open();
+                string sSQL = $"Insert into buglist (id, summary, description, type, priority, status, create_user, create_date) ";
+                sSQL += $"VALUES ('{DateTime.Now.ToString("yyyyMMddHHmmss")}', '{Summary}', '{Description}', '{Type}', '{Priority}', '{Status}','{sUser}','{DateTime.Now.ToString("yyyyMMdd")}')";
+                NpgsqlCommand cmd = new NpgsqlCommand(sSQL, conn);
+                cmd.ExecuteNonQuery();
+
+                BugModelList = GetBugLists();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View("Index", BugModelList);
+        }
+
+        public IActionResult Create_FeatureRequest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create_FeatureRequest(string Summary, string Description, string Type, string Priority, string Status)
+        {
+            List<BugListViewModel> BugModelList = new List<BugListViewModel>();
+            try
+            {
+                ViewData["User"] = memoryCache.Get(MemoryCacheKey.sUser);
+                string sUser = memoryCache.Get(MemoryCacheKey.sUser).ToString();
+
+                conn.Open();
+                string sSQL = $"Insert into buglist (id, summary, description, type, priority, status, create_user, create_date) ";
+                sSQL += $"VALUES ('{DateTime.Now.ToString("yyyyMMddHHmmss")}', '{Summary}', '{Description}', '{Type}', '{Priority}', '{Status}','{sUser}','{DateTime.Now.ToString("yyyyMMdd")}')";
+                NpgsqlCommand cmd = new NpgsqlCommand(sSQL, conn);
+                cmd.ExecuteNonQuery();
+
+                BugModelList = GetBugLists();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View("Index", BugModelList);
+        }
+
 
         [HttpPost]
         public IActionResult Index(string sUser)
@@ -137,7 +260,40 @@ namespace TicketTrackingSystem.Controllers
         public static class MemoryCacheKey
         {
             public static string sUserPermission = "UserPermission";
-            public static string sTest = "Test";
+            public static string sUser = "User";
+            public static string sBugID = "bugID";
+        }
+
+        public List<BugListViewModel> GetBugLists()
+        {
+            List<BugListViewModel> BugModelList = new List<BugListViewModel>();
+
+            NpgsqlCommand cmd = new NpgsqlCommand("select * from BugList", conn);
+
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+            DataSet _ds = new DataSet();
+            DataTable _dt = new DataTable();
+
+            da.Fill(_ds);
+            _dt = _ds.Tables[0];
+
+            for (int i = 0; i < _dt.Rows.Count; i++)
+            {
+                BugListViewModel BugListModel = new BugListViewModel();
+                BugListModel.ID = _dt.Rows[i]["id"].ToString();
+                BugListModel.Summary = _dt.Rows[i]["summary"].ToString();
+                BugListModel.Description = _dt.Rows[i]["description"].ToString();
+                BugListModel.Type = _dt.Rows[i]["type"].ToString();
+                BugListModel.Priority = _dt.Rows[i]["priority"].ToString();
+                BugListModel.Status = _dt.Rows[i]["status"].ToString();
+                BugListModel.CreateUser = _dt.Rows[i]["create_user"].ToString();
+                BugListModel.CreateDate = _dt.Rows[i]["create_date"].ToString();
+                BugListModel.EndUser = _dt.Rows[i]["end_user"].ToString();
+                BugListModel.EndDate = _dt.Rows[i]["end_date"].ToString();
+                BugModelList.Add(BugListModel);
+            }
+
+            return BugModelList;
         }
     }
 }
--- TicketTrackingSystem/Models/BugListViewModel.cs
+++ TicketTrackingSystem/Models/BugListViewModel.cs
@@ -11,7 +11,7 @@ namespace TicketTrackingSystem.Models
         public string Summary { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }//bug,Feature Request,Test Case
-        public string Priority { get; set; }//bug 1.2.3.4.5,Feature Request 0,Test Case 0
+        public string Priority { get; set; }//bug 1>2>3>4>5,Feature Request 0,Test Case 0
         public string Status { get; set; }//Processing,Resolved
         public string CreateUser { get; set; }//QA
         public string CreateDate { get; set; }
